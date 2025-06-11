d823ac14217b4b8a11029683416545f75057f03d
Kendal-Katherine <kendal.katherine@gmail.com>
2025-06-10 21:53:52 -0300

    Refactor main function to initialize router and remove unused code

--- cmd/main.go
+++ cmd/main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"math/rand"
+import "random-numbers/internal/router"
 
-	"github.com/gin-gonic/gin"
-)
-
-func main(){
-r := gin.Default()
-r.GET("/random", func(c *gin.Context) {
-	// Generate a random number and return it as JSON
-	c.JSON(200, gin.H{
-		"number": rand.Intn(100), // Random number between 0 and 99
-	})
-})
-
-r.Run(":8080")
-
+func main() {
+	router.Initialize()
 }
