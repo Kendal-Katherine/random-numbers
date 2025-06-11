e5798d1d6a57a8c310be529eff33b0279ae54e9a
Kendal-Katherine <kendal.katherine@gmail.com>
2025-06-10 21:15:25 -0300

    Refactor main package and add dependencies for random number generation API

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,20 @@
-package cmd
+package main
+
+import (
+	"math/rand"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main(){
+r := gin.Default()
+r.GET("/random", func(c *gin.Context) {
+	// Generate a random number and return it as JSON
+	c.JSON(200, gin.H{
+		"number": rand.Intn(100), // Random number between 0 and 99
+	})
+})
+
+r.Run(":8080")
+
+}
