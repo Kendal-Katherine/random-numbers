9ed58313cb60e8d305ee49ff099e19f56534eaab
Kendal-Katherine <kendal.katherine@gmail.com>
2025-06-11 00:00:14 -0300

    feat: Add SQLite initialization function to manage database creation and connection

---
+++ infrastructure/config/sqlite.go
@@ -0,0 +1,53 @@
+package config
+
+import (
+	"os"
+	"random-numbers/infrastructure/database"
+
+	"gorm.io/driver/sqlite"
+	gorm "gorm.io/gorm"
+)
+
+func InitializeSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := ".db/main.db"
+
+	//Check is the datavase file exists
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Infof("Database file does not exist, creating directory and file: %s", dbPath)
+
+		//Create the directory if it does not exist
+		err = os.MkdirAll(".db", os.ModePerm)
+		if err != nil {
+			logger.Errorf("Failed to create database directory: %v", err)
+			return nil, err
+		}
+
+		//Create the database file
+		file, err := os.Create(dbPath)
+		if err != nil {
+			logger.Errorf("Failed to create database file: %v", err)
+			return nil, err
+		}
+		defer file.Close()
+		logger.Infof("Database file created successfully: %s", dbPath)
+	}
+
+	// Initialize the SQLite database connection
+	logger.Infof("Initializing SQLite database connection...")
+	db, err := gorm.Open(sqlite.Open(".db/main.db"), &gorm.Config{})
+	if err != nil {
+		logger.Errorf("Failed to connect to the database: %v", err)
+		return nil, err
+	}
+	// Migrate the schema for the RandomNumber model
+	err = db.AutoMigrate(&database.RandomNumber{})
+	if err != nil {
+		logger.Errorf("Failed to migrate database schema: %v", err)
+		return nil, err
+	}
+	//Return the database connection
+	logger.Infof("SQLite database connection initialized successfully.")
+	return db, nil
+}
