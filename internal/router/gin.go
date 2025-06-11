749f8a1f86ab4a4f7f6fb73b7f43152780b3e49e
Kendal-Katherine <kendal.katherine@gmail.com>
2025-06-10 21:54:11 -0300

    Add router initialization and server setup in gin.go

---
+++ internal/router/gin.go
@@ -0,0 +1,15 @@
+package router
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func Initialize() {
+	//Initialize Router
+	r := gin.Default()
+
+	//Initialize Routes
+	initializeRoutes(r)
+	//Run Server
+	r.Run(":8080")
+}
